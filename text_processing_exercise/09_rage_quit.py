2c4042ec64d7d729b289c5052a51d8a11a3318b3
tidorino <tidorino@hotmail.com>
2022-04-02 20:44:34 +0300

    exercise done

--- text_processing_exercise/09_rage_quit.py
+++ text_processing_exercise/09_rage_quit.py
@@ -1,3 +1,14 @@
+# He'll give you a series of strings (containing only non-numerical characters) followed
+# by non-negative numbers (N), e.g., "a3". You need to convert the letters to uppercase
+# for each string and print it repeatedly N times on the console. In the example, you need to write
+# back "AAA".
+# First, on the output, you should print a statistic of the number of unique symbols used
+# (case-insensitive) in the format: "Unique symbols used {0}".
+# Next, print the rage message itself.
+# The strings and numbers will not be separated by anything. The input will always start with
+# a non-numeric symbol, and for each string, there will be a corresponding number. The input will
+# be given on a single line.
+
 def rage_quit(info):
     info = list(info)
     letters = ''
