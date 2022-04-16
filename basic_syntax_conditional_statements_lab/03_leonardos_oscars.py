d3de2ac03f2fd8db4388feb03c9d4ede000428a8
tidorino <tidorino@hotmail.com>
2022-04-16 18:13:14 +0300

    exercise done

---
+++ basic_syntax_conditional_statements_lab/03_leonardos_oscars.py
@@ -0,0 +1,10 @@
+numb = int(input())
+
+if numb == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif numb == 86:
+    print("Not even for Wolf of Wall Street?!")
+elif numb < 86 or numb == 87:
+    print("When will you give Leo an Oscar?")
+else:
+    print("Leo got one already!")
